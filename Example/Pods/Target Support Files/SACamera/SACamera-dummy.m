5e0c6f91a7c07a81f6ad2376dc26b6cea7cd2965
SATEESH <gudla.sateesh@practo.com>
2016-09-06 11:30:57 +0530

    swift 3.0 Syntax refactoring

---
+++ Example/Pods/Target Support Files/SACamera/SACamera-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SACamera : NSObject
+@end
+@implementation PodsDummy_SACamera
+@end
---
+++ Example/Pods/Target Support Files/SACamera/SACamera-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SACameraVersionNumber;
+FOUNDATION_EXPORT const unsigned char SACameraVersionString[];
+
